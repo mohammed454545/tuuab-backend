a98c2b5e7c6b5cf55f1bd59824676235bfec54a6
mohammed454545 <mohammed.h.salh@gmail.com>
2025-09-03 11:52:57 +0300

    port corecct

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+const port = parseInt(process.env.PORT ?? '5000', 10);
   // ✅ تفعيل CORS والسماح للواجهة بالاتصال
   app.enableCors({
     origin: 'http://localhost:3000', // أو ['http://localhost:3000'] إذا كان أكثر من واحد
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 5000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
